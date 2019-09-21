430669c97997aa276db29bc1fc7db8a929f36577
MattRoger <mrogerphotography@gmail.com>
2019-09-20 18:04:14 -0600

    started  js, can accept and input trains

--- assets/choochoo.js
+++ assets/choochoo.js
@@ -1,1 +1,43 @@
-// alert("I work")
+console.log("I work")
+var firebaseConfig = {
+    apiKey: "AIzaSyDRHwjxh4g0c2tdfOdkBqQ8I7J_KvShvNY",
+    authDomain: "test-6a2dd.firebaseapp.com",
+    databaseURL: "https://test-6a2dd.firebaseio.com",
+    projectId: "test-6a2dd",
+    storageBucket: "",
+    messagingSenderId: "664076151830",
+    appId: "1:664076151830:web:1cffef0fcf003deb2d9937"
+  };
+  firebase.initializeApp(firebaseConfig)
+  var database=firebase.database();
+
+
+$("#submit").on("click" ,function(event){
+    event.preventDefault();
+    console.log("clicked");
+    var trainName=$("#t-name").val().trim();
+    var destination=$("#destination").val().trim();
+    var firstTrain=$("#origin-arrive").val().trim();
+    var frequencyTrain=$("#frequency").val().trim();
+    database.ref().push({
+        name:trainName,
+        destination: destination,
+        start: firstTrain,
+        frequency:frequencyTrain,
+
+    })
+    
+})
+database.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val().name),
+
+    $("table").append($("<tr>").append(
+        $("<td>").text(childSnapshot.val().name),
+        $("<td>").text(childSnapshot.val().destination),
+        $("<td>").text(childSnapshot.val().start),
+        $("<td>").text(childSnapshot.val().frequency),
+        $("<td>").text("next train"),
+        $("<td>").text("mins away"),
+
+    ))
+})
